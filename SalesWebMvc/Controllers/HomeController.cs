85862c02fac742c71b732c77ef44e64554e733d0
Cherymoon <nemseialistar@gmail.com>
2020-03-18 11:13:22 -0300

    primeiros testes de razor e controladores

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales web MVC app from c#";
+            ViewData["Autor"] = "Guilherme Frankenberger";
 
             return View();
         }
