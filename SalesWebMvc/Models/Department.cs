79bbd1037e1e864d0421775fb45d1aeebc829246
Cherymoon <nemseialistar@gmail.com>
2020-03-19 12:13:48 -0300

    adição do seeding

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -14,11 +14,10 @@ namespace SalesWebMvc.Models
 
         public Department() {}
 
-        public Department(int id, string name, ICollection<Seller> sellers)
+        public Department(int id, string name)
         {
             Id = id;
             Name = name;
-            Sellers = sellers;
         }
 
         public void AddSeller(Seller seller)
